f2d61e87007dfab270e6415f656c4642fda5323d
17alma41 <17alma41@gmail.com>
2024-05-09 12:02:00 +0200

    change: name of eje12.sql to prueba.sql

--- prueba.sql
+++ prueba.sql
@@ -1,3 +1,2 @@
-SELECT * FROM usuarios WHERE id=20;
-SELECT * FROM productos WHERE id=123;
-SELECT * FROM productos WHERE id=32;
+SELECT * FROM usuarios WHERE rol='hacker';
+SELECT * FROM usuarios WHERE id=31;
